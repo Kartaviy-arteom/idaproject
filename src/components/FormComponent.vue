f3107bf97a5c575e6b795a294dc2a6ac13fbde3f
Artem Kartaviy <kag@sinergium.ru>
2022-02-28 21:09:07 +0300

    form button edit

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,8 +1,8 @@
 <template>
-  <form class="form" action="" method="post" autocomplete="off">
+  <form class="form" action="" method="post" autocomplete="off" @submit.prevent = "onSubmit">
     <p>
       <label for="product-name">Наименование товара</label>
-      <input type="text" name="product-name" id="product-name" required placeholder="Введите наименование товара">
+      <input type="text" name="product-name" id="product-name" required placeholder="Введите наименование товара" v-model="name">
     </p>
     <p>
       <label for="description">Описание товара</label>
@@ -10,12 +10,27 @@
     </p>
     <p>
       <label for="link">Ссылка на изображение товара</label>
-      <input type="text" name="link" id="link" required placeholder="Введите ссылку">
+      <input type="text" name="link" id="link" required placeholder="Введите ссылку" v-model="link">
     </p>
     <p>
       <label for="price">Цена товара</label>
-      <input type="text" name="price" id="price" required placeholder="Введите цену" pattern="[0-9.]+">
+      <input type="text" name="price" id="price" required placeholder="Введите цену" pattern="[0-9.]+" v-model="price">
     </p>
-    <button type="submit">Добавить товар</button>
+    <button type="submit" :disabled="!isFormValid">Добавить товар</button>
   </form>
 </template>
+
+<script>
+export default {
+  computed: {
+    isFormValid () {
+      return false
+    }
+  },
+  methods: {
+    // isFormValid () {
+    //   return !this.name
+    // }
+  }
+}
+</script>
